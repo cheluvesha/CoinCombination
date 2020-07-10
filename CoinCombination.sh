23230a55325989463a290ea58f6e3e4f7b337e4f
cheluvesha <cheluvesh.b@gmail.com>
2020-07-10 23:02:15 +0530

    Multiple time fliping the doublet coin

--- CoinCombination.sh
+++ CoinCombination.sh
@@ -1,32 +1,50 @@
 #!/bin/bash -x
 read -p "Enter the range to flip: " num
 val=1;
-count_head=0;
-count_tail=0;
-Key=0;
-declare -A HeadTail
+TailTail=0;
+HeadHead=0;
+HeadTail=0;
+TailHead=0;
+index=0;
 while [ $val -le $num ]
 do
-guess=$(( RANDOM% 2))
+guess=$(( 1+RANDOM% 4))
 if [ $guess -eq 1 ]
 then
-        array[((Key++))]="H"
-        ((count_head++))
+        arr[((index++))]="HH";
+        ((HeadHead++));
+elif [ $guess -eq 2 ]
+then
+        arr[((index++))]="TT";
+        ((TailTail++));
+elif [ $guess -eq 3 ]
+then    arr[((index++))]="HT";
+        ((HeadTail++));
 else
-        array[((Key++))]="T"
-        ((count_tail++))
+sa      arr[((index++))]="TH";
+        ((TailHead++))
 fi
 ((val++))
 done
-for((i=0;i<${#arr[@]};i++))
+echo ${arr[@]}
+
+declare -A Doublet
+for ((i=0;i<${#arr[@]};i++))
 do
-HeadTail[$i]=${arr[$i]};
+Doublet[$i]=${arr[$i]};
 done
 
-Head_Percen=`echo "$count_head*100/$num" | bc -l`
-Tail_Percen=`echo "$count_tail*100/$num" | bc -l`
+HH_Percen=`echo "$HeadHead*100/$num" | bc -l`
+TT_Percen=`echo "$TailTail*100/$num" | bc -l`
+HL_Percen=`echo "$HeadTail*100/$num" | bc -l`
+TH_Percen=`echo "$TailHead*100/$num" | bc -l`
 
-arr[0]=$Head_Percen;
-arr[1]=$Tail_Percen;
+declare -A PerStore
+PerStore[HH]=$HH_Percen;
+PerStore[TT]=$TT_Percen;
+PerStore[HL]=$HL_Percen;
+PerStore[TH]=$TH_Percen;
 
-echo ${arr[@]}
+for i in ${!PerStore[@]}
+do
+echo ":"${PerStore[$i]}
