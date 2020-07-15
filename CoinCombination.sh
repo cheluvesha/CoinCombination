1a21a0798cbfca73c95cab466392a5fa0d28cb3c
CHELUVESHA <38749165+cheluvesha@users.noreply.github.com>
2020-07-15 13:05:47 +0530

    Multiple time flipping a double coin

--- CoinCombination.sh
+++ CoinCombination.sh
@@ -1,4 +1,47 @@
 #!/bin/bash -x
+#UC1
+guess=$(( RANDOM% 2))
+if [ $guess -eq 1 ]
+then
+        echo "HEAD"
+else
+        echo "TAIL"
+fi
+
+#UC2
+read -p "Enter the range to flip: " num
+val=1;
+count_head=0;
+count_tail=0;
+Key=0;
+declare -A HeadTail
+while [ $val -le $num ]
+do
+guess=$(( RANDOM% 2))
+if [ $guess -eq 1 ]
+then
+        array[((Key++))]="H"
+        ((count_head++))
+else
+        array[((Key++))]="T"
+        ((count_tail++))
+fi
+((val++))
+done
+for((i=0;i<${#array[@]};i++))
+do
+HeadTail[$i]=${array[$i]};
+done
+
+Head_Percen=`echo "$count_head*100/$num" | bc -l`
+Tail_Percen=`echo "$count_tail*100/$num" | bc -l`
+declare -A arr
+arr[0]=$Head_Percen;
+arr[1]=$Tail_Percen;
+
+echo ${arr[@]}
+
+#UC3
 read -p "Enter the range to flip: " num
 val=1;
 TailTail=0;
